ee5111b329650344d53c1879591c08c0e7414c4f
Michael Beckler <beckler@hey.com>
2024-12-11 16:17:25 -0500

    feat: aero support

--- scripts/download-device-descriptors.ts
+++ scripts/download-device-descriptors.ts
@@ -1,17 +1,20 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { execSync } from 'child_process';
 
 const url =
 	'https://raw.githubusercontent.com/Pirate-MIDI/device-descriptors-api/main/device-descriptors/bridge-descriptors.json';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const outDir = path.join(__dirname, '../src/data');
 const outputPath = path.join(outDir, 'deviceDescriptors.ts');
 
 const banner = `
 /**
- * NOTE: this is downloaded and built from source on install, DO NOT modify. 
+ * NOTE: this is downloaded and built from source on install, DO NOT modify.
  * https://github.com/Pirate-MIDI/device-descriptors-api/blob/main/device-descriptors/bridge-descriptors.json
  */
 `.trim();
--- src/types/DeviceInfo.ts
+++ src/types/DeviceInfo.ts
@@ -1,6 +1,6 @@
 // Extend Record to help infer the runCommand type
 export interface DeviceInfo extends Record<string, unknown> {
-	deviceModel: 'Bridge6' | 'Bridge4' | 'Bridge1';
+	deviceModel: 'Bridge6' | 'Bridge4' | 'Aero';
 	firmwareVersion: string;
 	hardwareVersion: string;
 	uId: string;
